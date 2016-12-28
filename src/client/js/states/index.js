3ad0aa6272b46f14cdc916dfbaec24824af326d2
jpnsingh <jpnsingh86@gmail.com>
2016-12-28 14:10:13 +0530

    More refining the state provider;

--- src/client/js/states/index.js
+++ src/client/js/states/index.js
@@ -2,15 +2,35 @@
     'use strict';
 
     module.exports = angular.module('bcbsa-shell.states', [])
-        .config(function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
-                .state('home', {
+        .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+            var root = {
+                    name: 'root',
+                    url: '/',
+                    templateUrl: 'templates/partials/root.html'
+                },
+                home = {
+                    name: 'home',
                     url: '/home',
                     templateUrl: 'templates/partials/home.html'
-                })
-                .state('about', {
+                },
+                about = {
+                    name: 'about',
                     url: '/about',
                     templateUrl: 'templates/partials/about.html'
-                });
+                },
+                notFound = {
+                    name: 'notFound',
+                    url: '/404',
+                    templateUrl: 'templates/404.html'
+                };
+
+            $urlRouterProvider.when('', '/');
+
+            $locationProvider.html5Mode(true).hashPrefix('!');
+
+            $stateProvider
+                .state(root)
+                .state(home)
+                .state(about);
         });
 })();
