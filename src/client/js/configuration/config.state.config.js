5b0cd7f9d29056e24a5e6a3e6b9282cfc7154871
jpnsingh <jpnsingh86@gmail.com>
2017-01-05 16:57:31 +0530

    Refactoring state configs to follow above the fold pattern; Also, making the state change errors to be logged; Handling this in the default state run function;

--- src/client/js/configuration/config.state.config.js
+++ src/client/js/configuration/config.state.config.js
@@ -5,45 +5,49 @@
 
     configStateConfig.$inject = ['$stateProvider'];
     function configStateConfig($stateProvider) {
-        var configuration = {
-                name: 'configuration',
-                url: '/configuration',
-                abstract: true,
-                parent: 'nav',
-                templateUrl: 'templates/partials/configuration/configuration.html',
-                controller: 'ConfigCtrl',
-                controllerAs: 'configCtrl'
-            },
-            planSetup = {
-                name: 'configuration.planSetup',
-                url: '/planSetup',
-                docTitle: 'Configuration | Plan Setup',
-                templateUrl: 'templates/partials/configuration/plan.setup.html'
-            },
-            planAdditional = {
-                name: 'configuration.planAdditional',
-                url: '/planAdditional',
-                docTitle: 'Configuration | Plan Additional',
-                templateUrl: 'templates/partials/configuration/plan.additional.html'
-            },
-            featurePool = {
-                name: 'configuration.featurePool',
-                url: '/featurePool',
-                docTitle: 'Configuration | Feature Pool',
-                templateUrl: 'templates/partials/configuration/feature.pool.html'
-            },
-            featureAssignment = {
-                name: 'configuration.featureAssignment',
-                url: '/featureAssignment',
-                docTitle: 'Configuration | Feature Assignment',
-                templateUrl: 'templates/partials/configuration/feature.assignment.html'
-            };
-
         $stateProvider
-            .state(planSetup)
-            .state(planAdditional)
-            .state(featurePool)
-            .state(featureAssignment)
-            .state(configuration);
+            .state(states().planSetup)
+            .state(states().planAdditional)
+            .state(states().featurePool)
+            .state(states().featureAssignment)
+            .state(states().configuration);
+
+        function states() {
+            return {
+                configuration: {
+                    name: 'configuration',
+                    url: '/configuration',
+                    abstract: true,
+                    parent: 'nav',
+                    templateUrl: 'templates/partials/configuration/configuration.html',
+                    controller: 'ConfigCtrl',
+                    controllerAs: 'configCtrl'
+                },
+                planSetup: {
+                    name: 'configuration.planSetup',
+                    url: '/planSetup',
+                    docTitle: 'Configuration | Plan Setup',
+                    templateUrl: 'templates/partials/configuration/plan.setup.html'
+                },
+                planAdditional: {
+                    name: 'configuration.planAdditional',
+                    url: '/planAdditional',
+                    docTitle: 'Configuration | Plan Additional',
+                    templateUrl: 'templates/partials/configuration/plan.additional.html'
+                },
+                featurePool: {
+                    name: 'configuration.featurePool',
+                    url: '/featurePool',
+                    docTitle: 'Configuration | Feature Pool',
+                    templateUrl: 'templates/partials/configuration/feature.pool.html'
+                },
+                featureAssignment: {
+                    name: 'configuration.featureAssignment',
+                    url: '/featureAssignment',
+                    docTitle: 'Configuration | Feature Assignment',
+                    templateUrl: 'templates/partials/configuration/feature.assignment.html'
+                }
+            };
+        }
     }
 })();
--- src/client/js/states/default.state.config.js
+++ src/client/js/states/default.state.config.js
@@ -5,46 +5,6 @@
 
     defaultStateConfig.$inject = ['$urlMatcherFactoryProvider', '$urlRouterProvider', '$locationProvider', '$stateProvider'];
     function defaultStateConfig($urlMatcherFactoryProvider, $urlRouterProvider, $locationProvider, $stateProvider) {
-        var nav = {
-                name: 'nav',
-                abstract: true,
-                templateUrl: 'templates/partials/nav/navbar.html',
-                controller: 'NavBarCtrl',
-                controllerAs: 'navBarCtrl'
-            },
-            nonav = {
-                name: 'nonav',
-                abstract: true,
-                templateUrl: 'templates/partials/nav/nonav.html'
-            },
-            notFound = {
-                name: 'notFound',
-                url: '/404',
-                parent: 'nonav',
-                templateUrl: 'templates/404.html'
-            },
-            dashboard = {
-                name: 'dashboard',
-                url: '/dashboard',
-                docTitle: 'Dashboard',
-                parent: 'nav',
-                templateUrl: 'templates/partials/dashboard.html'
-            },
-            about = {
-                name: 'about',
-                url: '/about',
-                docTitle: 'About',
-                parent: 'nav',
-                templateUrl: 'templates/partials/about.html'
-            },
-            help = {
-                name: 'help',
-                url: '/help',
-                docTitle: 'Help',
-                parent: 'nav',
-                templateUrl: 'templates/partials/help.html'
-            };
-
         $urlMatcherFactoryProvider.caseInsensitive(true);
         $urlMatcherFactoryProvider.strictMode(false);
 
@@ -54,11 +14,55 @@
         $locationProvider.html5Mode(true);
 
         $stateProvider
-            .state(nav)
-            .state(nonav)
-            .state(notFound)
-            .state(dashboard)
-            .state(about)
-            .state(help);
+            .state(state().nav)
+            .state(state().nonav)
+            .state(state().notFound)
+            .state(state().dashboard)
+            .state(state().about)
+            .state(state().help);
+
+        function state() {
+            return {
+                nav: {
+                    name: 'nav',
+                    abstract: true,
+                    templateUrl: 'templates/partials/nav/navbar.html',
+                    controller: 'NavBarCtrl',
+                    controllerAs: 'navBarCtrl'
+                },
+                nonav: {
+                    name: 'nonav',
+                    abstract: true,
+                    templateUrl: 'templates/partials/nav/nonav.html'
+                },
+                notFound: {
+                    name: 'notFound',
+                    url: '/404',
+                    parent: 'nonav',
+                    templateUrl: 'templates/404.html'
+                },
+                dashboard: {
+                    name: 'dashboard',
+                    url: '/dashboard',
+                    docTitle: 'Dashboard',
+                    parent: 'nav',
+                    templateUrl: 'templates/partials/dashboard.html'
+                },
+                about: {
+                    name: 'about',
+                    url: '/about',
+                    docTitle: 'About',
+                    parent: 'nav',
+                    templateUrl: 'templates/partials/about.html'
+                },
+                help: {
+                    name: 'help',
+                    url: '/help',
+                    docTitle: 'Help',
+                    parent: 'nav',
+                    templateUrl: 'templates/partials/help.html'
+                }
+            };
+        }
     }
 })();
