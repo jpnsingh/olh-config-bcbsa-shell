57b6d5312a9ff25dd4e60f097fd661075c771a9b
jpnsingh <jpnsingh86@gmail.com>
2017-02-01 00:45:47 +0530

    Fixing lint error;

--- src/client/js/configuration/user/user.controller.js
+++ src/client/js/configuration/user/user.controller.js
@@ -4,8 +4,8 @@
     module.exports = angular.module('bcbsa-shell.config.user.controllers.userController', [])
         .controller('UserCtrl', UserCtrl);
 
-    UserCtrl.$inject = ['$timeout', 'auth', 'User', 'UserService', 'RoleService', 'ConfigFactory'];
-    function UserCtrl($timeout, auth, User, UserService, RoleService, ConfigFactory) {
+    UserCtrl.$inject = ['auth', 'User', 'UserService', 'RoleService', 'ConfigFactory'];
+    function UserCtrl(auth, User, UserService, RoleService, ConfigFactory) {
         var vm = this;
 
         vm.canModifyUsers = _.some(auth.currentUser().roles, {id: 'SuperUser'});
@@ -49,17 +49,15 @@
         vm.updateUser = function () {
             vm.updaing = true;
 
-            $timeout(function () {
-                UserService
-                    .updateUser(vm.selected)
-                    .then(function (data) {
-                        vm.updaing = false;
-                        vm.selected = data.user;
-                    }, function (error) {
-                        vm.updaing = false;
-                        vm.error = error;
-                    });
-            }, 5000);
+            UserService
+                .updateUser(vm.selected)
+                .then(function (data) {
+                    vm.updaing = false;
+                    vm.selected = data.user;
+                }, function (error) {
+                    vm.updaing = false;
+                    vm.error = error;
+                });
         };
 
         function init() {
