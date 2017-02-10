3e63a5baa358af4a380784df41b1caf3f33c212c
jpnsingh <jpnsingh86@gmail.com>
2017-02-10 18:28:28 +0530

    Unit test coverage;  - FeatureAssignmentCtrl;

---
+++ test/client/spec/js/configuration/plan/controllers/config.feature.assignment.controller.spec.js
@@ -0,0 +1,86 @@
+(function () {
+    'use strict';
+
+    describe('FeatureAssignmentCtrl', function () {
+        var scope,
+            ConfigService,
+            controller,
+            cachedConfig = {
+                featurePool: {
+                    appPool: [
+                        {
+                            displayName: {
+                                label: 'Display Name',
+                                type: 'text',
+                                placeholder: 'Display Name',
+                                value: ''
+                            },
+                            icon: {
+                                label: 'Icon/Image',
+                                type: 'image',
+                                placeholder: 'Icon/Image',
+                                value: '',
+                                src: ''
+                            },
+                            appName: {
+                                label: 'App Name',
+                                type: 'text',
+                                placeholder: 'App Name',
+                                value: ''
+                            },
+                            appType: {
+                                label: 'App Type',
+                                type: 'text',
+                                placeholder: 'App Type',
+                                value: ''
+                            },
+                            appId: {
+                                label: 'App ID',
+                                type: 'text',
+                                placeholder: 'App ID',
+                                value: ''
+                            },
+                            appUrl: {
+                                label: 'App URL',
+                                type: 'text',
+                                placeholder: 'App URL',
+                                value: ''
+                            },
+                            storeUrl: {
+                                label: 'Store URL',
+                                type: 'text',
+                                placeholder: 'Store URL',
+                                value: ''
+                            },
+                            enabled: {
+                                label: 'Enabled',
+                                type: 'checkbox',
+                                placeholder: 'Enabled',
+                                value: false
+                            }
+                        }
+                    ]
+                }
+            };
+
+        beforeEach(angular.mock.module('bcsba-shell.configuration.plan.controllers.featureAssignmentController'));
+
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+
+            ConfigService = {
+                getCachedConfig: jasmine.createSpy('getCachedConfig').and.returnValue(cachedConfig)
+            };
+
+            controller = $controller('FeatureAssignmentCtrl', {
+                $scope: scope,
+                ConfigService: ConfigService
+            });
+        }));
+
+        it('should initialize the controller accordingly', function () {
+            expect(ConfigService.getCachedConfig).toHaveBeenCalled();
+            expect(controller.rootConfig).toEqual(cachedConfig);
+        });
+    });
+})();
