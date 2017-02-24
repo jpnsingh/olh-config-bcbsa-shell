caedc359f3b87848c8af98bfaaaee7e25da1119b
jpnsingh <jpnsingh86@gmail.com>
2017-02-24 16:30:06 +0530

    Translating Configuration sidebar;

--- src/client/js/configuration/configuration.controller.js
+++ src/client/js/configuration/configuration.controller.js
@@ -9,10 +9,10 @@
         var vm = this;
 
         vm.sidebar = [
-            {title: 'Role Management', state: 'configuration.role', activeState: 'configuration.role'},
-            {title: 'User Management', state: 'configuration.user', activeState: 'configuration.user'},
-            {title: 'Plan Management', state: 'configuration.plan.setup', activeState: 'configuration.plan'},
-            {title: 'Reports', state: 'configuration.report', activeState: 'configuration.report'}
+            {title: 'config.sidebar.roleManagementLabel', state: 'configuration.role', activeState: 'configuration.role'},
+            {title: 'config.sidebar.userManagementLabel', state: 'configuration.user', activeState: 'configuration.user'},
+            {title: 'config.sidebar.planManagementLabel', state: 'configuration.plan.setup', activeState: 'configuration.plan'},
+            {title: 'config.sidebar.reportsLabel', state: 'configuration.report', activeState: 'configuration.report'}
         ];
     }
 })();
