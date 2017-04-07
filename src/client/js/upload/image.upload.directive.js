3f5552b281c256691fc44ab584aa1a0d47b30d49
jpnsingh <jpnsingh86@gmail.com>
2017-04-07 18:48:21 +0530

    Converting image upload directive into es6 module;

--- src/client/js/upload/image.upload.directive.js
+++ src/client/js/upload/image.upload.directive.js
@@ -1,43 +1,40 @@
-(function () {
-    'use strict';
+'use strict';
 
-    module.exports = angular.module('bcbsa-shell.upload.directives.fileUploadButtonDirective', [])
-        .directive('imageUpload', UploadButtonDirective);
+export function ImageUploadDirective(FileUploaderService, NotificationService) {
+    const directiveDefinitionObject = {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+            fileModel: '=',
+            hide: '='
+        },
+        templateUrl: 'templates/partials/upload/image.upload.html'
+    };
 
-    UploadButtonDirective.$inject = ['FileUploaderService', 'NotificationService'];
-    function UploadButtonDirective(FileUploaderService, NotificationService) {
-        return {
-            restrict: 'EA',
-            replace: true,
-            scope: {
-                fileModel: '=',
-                hide: '='
-            },
-            templateUrl: 'templates/partials/upload/image.upload.html',
-            link: link
+    directiveDefinitionObject.link = (scope/*, element, attrs, ctrl*/) => {
+        scope.uploadFile = function (file) {
+            if (!file) {
+                return;
+            }
+
+            scope.uploading = true;
+
+            FileUploaderService
+                .uploadFile(file)
+                .then(function (data) {
+                    scope.uploading = false;
+                    scope.fileModel.src = 'data:' + data.file.headers['content-type'] + ';base64,' + data.file.base64String;
+                    scope.fileModel.value = data.file.originalFilename;
+                }, function (error) {
+                    scope.uploading = false;
+                    NotificationService.displayError(error.message);
+                }, function (progress) {
+                    scope.uploadProgress = progress;
+                });
         };
+    };
 
-        function link(scope/*, element, attrs, ctrl*/) {
-            scope.uploadFile = function (file) {
-                if (!file) {
-                    return;
-                }
+    return directiveDefinitionObject;
+}
 
-                scope.uploading = true;
-
-                FileUploaderService
-                    .uploadFile(file)
-                    .then(function (data) {
-                        scope.uploading = false;
-                        scope.fileModel.src = 'data:' + data.file.headers['content-type'] + ';base64,' + data.file.base64String;
-                        scope.fileModel.value = data.file.originalFilename;
-                    }, function (error) {
-                        scope.uploading = false;
-                        NotificationService.displayError(error.message);
-                    }, function (progress) {
-                        scope.uploadProgress = progress;
-                    });
-            };
-        }
-    }
-})();
+ImageUploadDirective.$inject = ['FileUploaderService', 'NotificationService'];
--- src/client/js/upload/index.js
+++ src/client/js/upload/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 import {FileUploaderService} from './file.upload.service';
+import {ImageUploadDirective} from './image.upload.directive';
 
 module.exports = angular
-    .module('bcbsa-shell.upload', [
-        'ngFileUpload',
-        require('./image.upload.directive.js').name
-    ])
-    .service('FileUploaderService', FileUploaderService);
+    .module('bcbsa-shell.upload', ['ngFileUpload'])
+    .service('FileUploaderService', FileUploaderService)
+    .directive('imageUpload', ImageUploadDirective);
