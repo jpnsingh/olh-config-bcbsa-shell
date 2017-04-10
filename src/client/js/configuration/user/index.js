dc8ad68f9bed3a634bdad41d4b11896cd473267c
jpnsingh <jpnsingh86@gmail.com>
2017-04-10 17:16:20 +0530

    Renaming config user files accordingly;

--- src/client/js/configuration/user/index.js
+++ src/client/js/configuration/user/index.js
@@ -3,8 +3,8 @@
 
     module.exports = angular
         .module('bcsba-shell.configuration.user', [
-            require('./user.factory').name,
-            require('./user.service').name,
-            require('./user.controller').name
+            require('./config.user.factory.js').name,
+            require('./config.user.service.js').name,
+            require('./config.user.controller.js').name
         ]).config(require('./config.user.state.config'));
 })();
