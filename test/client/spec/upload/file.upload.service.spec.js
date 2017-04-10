863a230db131b49d7d939459a951307c36a48aa4
jpnsingh <jpnsingh86@gmail.com>
2017-04-10 12:14:06 +0530

    Specs for file upload service;

--- test/client/spec/upload/file.upload.service.spec.js
+++ test/client/spec/upload/file.upload.service.spec.js
@@ -5,12 +5,17 @@ import {FileUploaderService} from '../../../../src/client/js/upload/file.upload.
 describe('FileUploaderService:', () => {
     let q,
         deferred,
+        timeout,
         Upload,
+        fileToUpload,
+        uploadSuccessResponse,
+        uploadErrorResponse,
         service;
 
-    beforeEach(inject(function ($q) {
+    beforeEach(inject(($q, $timeout) => {
         q = $q;
         deferred = q.defer();
+        timeout = $timeout;
     }));
 
     beforeEach(() => {
@@ -20,6 +25,25 @@ describe('FileUploaderService:', () => {
             upload: ''
         };
 
+        fileToUpload = {
+            fileName: 'test.test'
+        };
+
+        uploadSuccessResponse = {
+            data: {
+                file: {
+                    originalFileName: 'test.png',
+                    base64String: 'some,Random,base64String'
+                }
+            }
+        };
+
+        uploadErrorResponse = {
+            data: {
+                error: 'Error'
+            }
+        };
+
         spyOn(Upload, 'upload').and.returnValue(deferred.promise);
 
         service = new FileUploaderService(Upload);
@@ -27,9 +51,28 @@ describe('FileUploaderService:', () => {
 
     describe('uploadFile', () => {
         it('should respond with the uploaded file in success', () => {
-            service.uploadFile({fileName: 'test.test'});
+            service.uploadFile(fileToUpload);
 
-            expect(Upload.upload).toHaveBeenCalled();
+            expect(Upload.upload).toHaveBeenCalledWith({url: 'api/upload/file', file: fileToUpload});
+
+            deferred.resolve(uploadSuccessResponse);
+
+            timeout.flush();
+
+            expect(deferred.promise).toResolveWith(uploadSuccessResponse);
+            expect(uploadSuccessResponse.data.file.base64String).toEqual('some,Random,base64String');
+        });
+
+        it('should handle the error accordingly', () => {
+            service.uploadFile(fileToUpload);
+
+            expect(Upload.upload).toHaveBeenCalledWith({url: 'api/upload/file', file: fileToUpload});
+
+            deferred.reject(uploadErrorResponse);
+
+            timeout.flush();
+
+            expect(deferred.promise).toRejectWith(uploadErrorResponse);
         });
     });
 });
